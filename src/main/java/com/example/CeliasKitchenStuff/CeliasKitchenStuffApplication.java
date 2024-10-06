6e635261eff5f7af681eeb6c472a767248f04c74
Celia May <celiasophie02@gmail.com>
2024-10-06 17:23:37 +0200

    first commit

---
+++ src/main/java/com/example/CeliasKitchenStuff/CeliasKitchenStuffApplication.java
@@ -0,0 +1,13 @@
+package com.example.CeliasKitchenStuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeliasKitchenStuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeliasKitchenStuffApplication.class, args);
+	}
+
+}
